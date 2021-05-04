bf8ac8ced65d6b903556b19baad7bf0fc5951733
sourabh60 <bubanalesourabh60@gmail.com>
2021-05-04 15:11:03 +0530

    added f-string

--- bmiCalculator.py
+++ bmiCalculator.py
@@ -2,15 +2,15 @@ height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 
 bmi= weight / (height*height)
-
 bmii=round(bmi,2)
+
 if(bmii <=18):
-  print(f"Your BMI is"+str(bmii)+" , you are underweight.")
+  print(f"Your BMI is {bmii}, you are underweight.")
 elif(bmii<=22):
-  print("Your BMI is"+str(bmii)+" , you are normal.")
+  print(f"Your BMI is {bmii}, you are normal.")
 elif(bmii<=28):
-  print("Your BMI is"+str(bmii)+" , you are slightly overweight.")
+  print(f"Your BMI is {bmii}, you are slightly overweight.")
 elif(bmii<=22):
-  print("Your BMI is"+str(bmii)+" , you are obese.")
+  print(f"Your BMI is {bmii}, you are obese.")
 else:
-  print("Your BMI is"+bmii+" , you are clinically obese.")
+  print(f"Your BMI is {bmii}, you are clinically obese.")
