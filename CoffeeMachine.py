33de5028a887c27cd55c687ae823667736eed053
sourabh60 <bubanalesourabh60@gmail.com>
2021-06-06 13:39:49 +0530

    virtual coffee machine

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -61,7 +61,7 @@ def is_transaction_succesful(money_received,drink_cost):
         profit += drink_cost
         return True
     else:
-        print("Sorry that's not enough money. Money refunded.")
+        print("Sorry that's not enough money. Money refunded. ")
         return False
 
 
