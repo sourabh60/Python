81bb5fe23141d571868006feab387215516f4338
sourabh60 <bubanalesourabh60@gmail.com>
2021-06-08 14:30:44 +0530

    Used OOP

--- Quiz-Game/main.py
+++ Quiz-Game/main.py
@@ -9,9 +9,8 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
-print("you have completed the quiz")
+print("you have completed the quiz.")
 print(f"Your final score was :{quiz.score}/{quiz.question_number}")
