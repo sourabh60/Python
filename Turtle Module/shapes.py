283af85ea1e5c9b89580f2dc8f1252be0710694f
sourabh60 <bubanalesourabh60@gmail.com>
2021-06-11 12:45:37 +0530

    Used turtle module which draws 8 different shapes in a row.

--- Turtle Module/shapes.py
+++ Turtle Module/shapes.py
@@ -2,7 +2,7 @@ from turtle import Turtle, Screen
 import random
 
 tim = Turtle()
-colours = ["CornflowerBlue","DarkOrchid", "IndianRed"]
+colours = ["CornflowerBlue","DarkOrchid", "IndianRed","SlateGray"]
 def draw_shape(num_sides):
     angle = 360/ num_sides
     for  i in range(num_sides):
