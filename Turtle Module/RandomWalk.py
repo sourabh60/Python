00ce1e581e2603f48fb6331fef189bfa2a48b019
sourabh60 <bubanalesourabh60@gmail.com>
2021-06-11 12:42:58 +0530

    turtle module

---
+++ Turtle Module/RandomWalk.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+colours = ["CornflowerBlue","DarkOrchid", "IndianRed"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+for i in range(200):
+    tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+screen = Screen
+screen.exitonclick()
